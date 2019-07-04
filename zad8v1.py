15980af9d06499a692a643950eb2cf78246a9c77
Radek <rgalazka@gmail.com>
2019-07-04 17:02:04 +0200

    zmiana zad 8

--- zad8v1.py
+++ zad8v1.py
@@ -14,15 +14,26 @@ while stat != 'k':
                         Nożyce - n\n   
                     """)
     if usr_var == 'p':
-        usr_var = 0;
-    elif usr_var == 'k':
-        usr_var = 1;
-    elif usr_var == 'n':
         usr_var = 2;
+    elif usr_var == 'k':
+        usr_var = 0;
+    elif usr_var == 'n':
+        usr_var = 1;
     else:
-        print("Nie podałeś poprawnej wartości !!!"); break
+        print("Nie podałeś poprawnej wartości !!!");
+        break
 
     rnd_var = rnd(0, 2)
-    print(usr_var)
-    print(rnd_var)
+
+    if usr_var == 0 and rnd_var == 1 or usr_var == 1 and rnd_var == 2 or usr_var == 2 and rnd_var == 0:
+        print(f'''
+        WYGRAŁEŚ
+        Komputer - {MZ[rnd_var]} 
+           Gracz - {MZ[usr_var]}
+        ''')
+    elif usr_var == rnd_var:
+        print('Remis')
+    else:
+        print('Przegrałeś')
+
     stat = input('Gramy ? (k-koniec):')
